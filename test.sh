9feb5c28b77e4f1094e20687dec53d8c6455126c
Mehmet Ali Baykara <mehmet.baykara@sva.de>
2023-12-23 11:40:51 +0100

    init

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env
-#assume this script will be executed in a git project
- URL=$(cat .git/config | grep url| cut -d\  -f3)
- rm -rf .git
- git init
- git add .
- git commit -m "Initial commit"
- git remote add origin $URL
- git push -u --force origin main
